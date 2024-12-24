ffc93b4c43968082c6b3604576d1e681bbde6a3a
Diogo Ribeiro <slysherz@gmail.com>
2024-12-24 20:55:26 +0000

    solve rest of day 7

--- day7.py
+++ day7.py
@@ -1,27 +1,50 @@
-import sys
 import re
 import shared
+
 
 def parse_input():
     input = shared.get_input()
     result = []
-    for m in re.finditer(r'(\d+): ([\d ]+)', input):
+    for m in re.finditer(r"(\d+): ([\d ]+)", input):
         r, ns = m.groups()
-        result.append((int(r), [int(s) for s in ns.split(' ')]))
+        result.append((int(r), [int(s) for s in ns.split(" ")]))
     return result
 
-def solvable(r, ns):
+
+def concat(a, b):
+    return int(str(a) + str(b))
+
+
+def solvable1(r, ns):
     possibilities = set([ns[0]])
     for n in ns[1:]:
-        possibilities = set(n + c for c in possibilities) | set(n * c for c in possibilities)
+        possibilities = set(n + c for c in possibilities) | set(
+            n * c for c in possibilities
+        )
 
     return r in possibilities
+
+
+def solvable2(r, ns):
+    possibilities = set([ns[0]])
+    for n in ns[1:]:
+        possibilities = (
+            set(n + c for c in possibilities)
+            | set(n * c for c in possibilities)
+            | set(concat(c, n) for c in possibilities)
+        )
+
+    return r in possibilities
+
 
 def main():
     input = parse_input()
 
-    result = sum(1 if solvable(n, ns) else 0 for n, ns in input)
-    print(f"Result part 1: {result}")
+    result1 = sum(n if solvable1(n, ns) else 0 for n, ns in input)
+    result2 = sum(n if solvable2(n, ns) else 0 for n, ns in input)
+    print(f"Result part 1: {result1}")
+    print(f"Result part 2: {result2}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
